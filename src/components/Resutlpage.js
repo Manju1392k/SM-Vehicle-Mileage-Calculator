0ae1f5faf9b79f527a506abbc5aab4592d7a69ac
Manjunath <sreerammanjunath1392@gmail.com>
2024-03-01 22:21:13 +0530

    Version12

--- src/components/Resutlpage.js
+++ src/components/Resutlpage.js
@@ -1,7 +1,28 @@
-import React from 'react'
+
+import React, { useState, useEffect } from "react";
 
 export default function Resutlpage() {
+  const [vehicleData, setVehicleData] = useState(null);
+    
+  useEffect(() => {
+    // Get data from localStorage and set it to state
+    const storedData = localStorage.getItem('VehiclData');
+    if (storedData) {
+      // Parse the stringified data back into an object
+      setVehicleData(JSON.parse(storedData));
+    }
+  }, []);
   return (
-    <div>Resutlpage</div>
+    <>
+    {vehicleData ? 
+      <div>
+      <p>Vehicle Name: {vehicleData.Perlitercost}</p>
+      <p>Vehicle Model: {vehicleData.Firstmeterreading}</p>
+      {/* Add more fields as necessary */}
+    </div>
+    :
+    <div>Fill the details properly</div>
+    }
+    </>
   )
 }
