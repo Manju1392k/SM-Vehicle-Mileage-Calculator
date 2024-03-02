08c0112c42aa2cf7eff721f7c6561f221d5be0c4
Manjunath <sreerammanjunath1392@gmail.com>
2024-03-02 20:45:48 +0530

    newversion4

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,43 +1,61 @@
+
 import React, { useState } from 'react';
 
+import { ToastContainer, toast } from 'react-toastify';
+
 function Home() {
-  const [VehicleData, setVehicleData] = useState({
-    vehicletype: '',
-    fueltype: '',
-    FuelPrice: '',
-    TotalAmount: '',
-    FuelDensity: '',
-    FirstFuelPoint: '',
-    SecondFuelPoint: '',
-  });
 
-  const handleChange = (event) => {
-    setVehicleData({
-      ...VehicleData,
-      [event.target.name]: event.target.value
+  // Wrapping total code in 'try & catch to avoid crashes'.
+  try {
+    // Using var becuase var did not have block scope. But let & const have block scope.
+    var [VehicleData, setVehicleData] = useState({
+      vehicletype: '',
+      fueltype: '',
+      FuelPrice: '',
+      TotalAmount: '',
+      FuelDensity: '',
+      FirstFuelPoint: '',
+      SecondFuelPoint: '',
     });
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem('VehicleData', JSON.stringify(VehicleData));
-  };
+    // Function to change and work the inputs with keyboard.
+    var handleChange = (event) => {
+      setVehicleData({
+        ...VehicleData,
+        [event.target.name]: event.target.value
+      });
+    };
 
+    // Function to change and work the inputs with keyboard. And to Store 'VehicleData' in localstorage
+    var handleSubmit = (event) => {
+      event.preventDefault();
+      localStorage.setItem('VehicleData', JSON.stringify(VehicleData));
+    };
+
+  } catch (error) {
+    toast.info("Something went wrong. Please try later")
+  };
   return (
     <>
+      <ToastContainer />
+
+      {/* Div for Heading */}
       <div className="w-full grid justify-center">
         <h1 className="text-3xl bg-white w-max py-4 px-4 mt-3 rounded-md font-bold">SM Vehicle Mileage Calculator</h1>
 
+        {/* Div for form background */}
         <div className=" bg-white w-[95vw] py-4 px-4 my-3 rounded-md ">
           <form className="flex flex-col items-start" onSubmit={handleSubmit}>
 
+            {/* Select menu for To Choose Vehicle Type */}
             <select className="my-2 bg-[#7741f6] text-white font-semibold py-2 px-2 rounded-md outline-none" name="vehicletype" onChange={handleChange} required={true}>
               <option value="Not Selected">Choose Vehicle</option>
               <option value="2 Wheeler">2 Wheeler</option>
-              <option value="female">4 Wheeler</option>
+              <option value="4 Wheeler">4 Wheeler</option>
               <option value="Other Vehicle">Other Vehicle</option>
             </select>
 
+            {/* Select menu for To Choose Fuel Type */}
             <select className="my-2 bg-[#7741f6] text-white font-semibold py-2 px-2 rounded-md outline-none" name="fueltype" onChange={handleChange} required={true}>
               <option value="Not Selected">Choose Fuel</option>
               <option value="Petrol">Petrol</option>
@@ -45,21 +63,28 @@ function Home() {
               <option value="Other Fuel">Other Fuel</option>
             </select>
 
+            {/* Input for to take Pertol or Diesel per liter cost */}
             <h1 className='font-semibold text-xl'>Fuel Per liter Cost</h1>
             <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="FuelPrice" onChange={handleChange} placeholder="Per liter of fuel" required />
 
-            <h1 className='font-semibold text-xl'>Total Refil of Fuel Amout</h1>
+            {/* Input for to take how much price pertol or diesel is filled in tank */}
+            <h1 className='font-semibold text-xl'>Total Refill of Fuel Amout</h1>
             <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="TotalAmount" onChange={handleChange} placeholder="Total Amount in Rupess" required />
 
+            {/* Input for to take the pertol or diesel density */}
             <h1 className='font-semibold text-xl'>Fuel Density</h1>
-            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="FuelDensity" onChange={handleChange} placeholder="Fuel Density (optional)" required />
+            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="FuelDensity" onChange={handleChange} placeholder="Fuel Density (optional)" />
 
+            {/* Input for to take the first fuel stop point meter reading */}
             <h1 className='font-semibold text-xl'>First Fuel Point Meter Reading</h1>
-            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="FirstFuelPoint" onChange={handleChange} placeholder="First Fuel Point Meter Reading. Enter First Refil Fuel Meter Reading" required />
+            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="FirstFuelPoint" onChange={handleChange} placeholder="First Fuel Point Meter Reading. Enter First Refill Fuel Meter Reading" required />
 
+
+            {/* Input for to take the Second fuel stop point meter reading */}
             <h1 className='font-semibold text-xl'>Second Fuel Point Meter Reading</h1>
-            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="SecondFuelPoint" onChange={handleChange} placeholder="Second Fuel Point Meter Reading. Enter Second Refil Fuel Meter Reading" required />
+            <input className="mb-2 py-2 px-2 rounded-md w-full outline-none border-2 focus:border-[#7741f6]" type="tel" name="SecondFuelPoint" onChange={handleChange} placeholder="Second Fuel Point Meter Reading. Enter Second Refill Fuel Meter Reading" required />
 
+            {/* Button for submitting VehicleData to localStorage */}
             <button className="bg-[#7741f6] py-2 px-6 rounded-lg text-white font-semibold hover:bg-[#8b5aff] active:bg-[#592bc3]" type="submit">Show Results</button>
 
           </form>
