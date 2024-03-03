f2ddd4bedeae4088fd073bc874164241b4abc857
Manjunath <sreerammanjunath1392@gmail.com>
2024-03-03 13:38:59 +0530

    newversion11

--- src/components/Resutlpage.js
+++ src/components/Resutlpage.js
@@ -8,10 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 import moment from "moment-timezone";
 
 export default function Resutlpage() {
-  try {
-  } catch (error) {
-    toast("Something went wrong. Please try later");
-  }
 
   // Get time and date from moment.
   const [datetime, setDatetime] = useState(moment().tz("Asia/Kolkata"));
@@ -28,6 +24,28 @@ export default function Resutlpage() {
     }
   }, []);
 
+  const [vehicleData, setVehicleData] = useState({});
+
+  useEffect(() => {
+    try {
+      // Get the data from localStorage
+      const storedVehicleData = localStorage.getItem('VehicleData');
+
+      // Parse the data and set it to state
+      if (storedVehicleData) {
+        setVehicleData(JSON.parse(storedVehicleData));
+      }
+    } catch (error) {
+      toast("Something went wrong. Please try later");
+    }
+  }, []);
+
+  console.log(vehicleData)
+
+  var FuelType = vehicleData.fueltype
+
+  var TotalMileage = vehicleData.SecondFuelPoint - vehicleData.FirstFuelPoint
+
   return (
     <>
       <ToastContainer position="top-center" />
@@ -41,6 +59,24 @@ export default function Resutlpage() {
         <div className=" bg-white w-[95vw] py-4 px-4 my-3 rounded-md ">
           <h1 className="text-xl font-semibold">
             Date & Time: <span className="font-bold">{datetime.format('MMMM Do YYYY, h:mm a')}</span>
+          </h1>
+          <h1 className="text-lg mt-4 font-semibold">
+            Vehicle Type: <span className="font-bold">{vehicleData.vehicletype}</span>
+          </h1>
+          <h1 className="text-lg mt-1 font-semibold">
+            Fuel Type: <span className="font-bold">{FuelType}</span>
+          </h1>
+          <h1 className="text-lg mt-1 font-semibold">
+            {FuelType} Per Liter Cost  : <span className="font-bold">{vehicleData.FuelPrice} Rs</span>
+          </h1>
+          <h1 className="text-lg mt-1 font-semibold">
+            Total Amount of {FuelType} fill in tank: <span className="font-bold">{vehicleData.TotalAmount} Rs</span>
+          </h1>
+          <h1 className="text-lg mt-1 font-semibold">
+            {FuelType} Density: <span className="font-bold">{vehicleData.FuelDensity}</span>
+          </h1>
+          <h1 className="text-lg mt-1 font-semibold">
+            Total Mileage by {vehicleData.vehicletype}: <span className="font-bold">{TotalMileage.toFixed(2)} kms</span>
           </h1>
         </div>
       </div>
